3820bae3b90ac0412b81f25b7b56dafbb51dc533
maxwell3025 <max@itarchi.com>
2019-09-24 19:02:25 -0400

    initial draft

---
+++ src/sandboxbattle/Sand.java
@@ -0,0 +1,48 @@
+package sandboxbattle;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JFrame;
+
+public class Sand extends JFrame implements Runnable, MouseListener{
+
+	public Sand() {
+		
+	}
+
+	public static void main(String[] args) {
+	
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
